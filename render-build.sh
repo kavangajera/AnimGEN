4e8b9744834985b89053cfa1dd0ed65a2637b698
Devarshi2285 <134229662+Devarshi2285@users.noreply.github.com>
2025-02-22 16:58:46 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+# Update packages
 sudo apt update
-sudo apt install -y ffmpeg texlive
+
+# Install required dependencies
+sudo apt install -y ffmpeg texlive texlive-latex-extra ghostscript
+
+# Upgrade pip
+pip install --upgrade pip
