f21b8c99d321e42154e98032dd3a8d5d01a6ab4f
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-02-23 10:45:41 +0530

    changed render

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, use_reloader=False)
